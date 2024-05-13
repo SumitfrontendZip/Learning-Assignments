4f4c11d6b0832477e302611020148b5f81b7cd6b
Sumit-Kumar <sumit@frontendzip.com>
2024-05-13 11:38:35 +0530

    Learn null is object or not

--- selfmadeQuestions.js
+++ selfmadeQuestions.js
@@ -12,3 +12,14 @@ let b = new Number(5);
 
 console.log(a==b);  // true
 console.log(a===b);  // false
+
+// null is object or not ----
+
+const check = typeof null == Object
+console.log(check)  // false
+
+// what is type of null
+
+console.log(typeof null);
+
+
