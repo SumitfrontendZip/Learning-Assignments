a1ce715b072ce60393969725c647954284f741a2
Sumit-Kumar <sumit@frontendzip.com>
2024-05-14 18:27:28 +0530

    bind and call method

--- selfmadeQuestions.js
+++ selfmadeQuestions.js
@@ -209,3 +209,30 @@
   
 //   getAge([]);
 
+
+
+////////////////////////////////////////////////////////////////
+
+
+// function getAge() {
+//     'use strict';
+//     age = 21;
+//     console.log(age);
+//   }
+  
+//   getAge();
+
+
+// console.log(eval('10*10+5'));
+
+// sessionStorage.setItem('cool_secret', 123);
+
+
+const person = { name: 'Lydia' };
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+
+// console.log(sayHi.call(person, 21));
+// console.log(sayHi.bind(person, 21));
